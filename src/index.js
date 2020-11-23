2737c2b3876f681c5c213b0f58e7d1ca57133927
alekszone <50597224+alekszone@users.noreply.github.com>
2020-11-23 17:00:38 +0100

    index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {Provider} from "react-redux"
 import configureStore from './store';
 import { BrowserRouter } from 'react-router-dom';
